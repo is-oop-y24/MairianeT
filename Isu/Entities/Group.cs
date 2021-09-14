0830a587856de9ffb121731b411a17fa2b5ed8a1
MairianeT <MairianeT@users.noreply.github.com>
2021-09-14 21:27:28 +0300

    started working on this project

---
+++ Isu/Entities/Group.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Isu.Entities
+{
+    public class Group
+    {
+        public Group()
+        {
+        }
+    }
+}
---
+++ Isu/Services/IsuService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Isu.Services
+{
+    public class IsuService : IIsuService
+    {
+        public Group AddGroup(string name)
+        {
+
+        }
+
+        public Student AddStudent(Group group, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeStudentGroup(Student student, Group newGroup)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group FindGroup(string groupName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Group> FindGroups(CourseNumber courseNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student FindStudent(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Student> FindStudents(string groupName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Student> FindStudents(CourseNumber courseNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetStudent(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
