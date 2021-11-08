17a3acb1e5884cbc73eb1872e7b625bc8fd2df62
MairianeT <MairianeT@users.noreply.github.com>
2021-11-08 23:06:11 +0300

    fix

--- Backups/IBackup.cs
+++ Backups/IBackup.cs
@@ -5,7 +5,9 @@
         void Add(string filePath, string fileName);
         void Remove(string filePath, string fileName);
         void MakeRestorePoint();
+        void MakeVirtualBackup();
         bool CheckRestorePoint(int restorePointNumber, int filesNumber);
+        bool CheckVirtualBackup(int virtualRestorePointNumber, string filePath);
         bool IsFileHere(int restorePointNumber, string filePath);
     }
 }
